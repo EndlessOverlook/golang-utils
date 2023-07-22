3486eac598b0d894dda2118149f1bfce39ffc625
EndlessOverlook <endlessoverlook@sina.com>
2023-07-22 21:39:21 +0800

    :hammer: 增加数组遍历函数和计算平均值函数

--- main/main.go
+++ main/main.go
@@ -15,6 +15,28 @@ func verifyGolangPackageName() {
 	fmt.Println("Age is : " + strconv.Itoa(age_utils.GetAge()))
 }
 
+// 遍历数组
+func iterateArray() {
+	var notes [7]string = [7]string{"do", "re", "mi", "fa", "so", "la", "xi"}
+	for i, n := range notes {
+		fmt.Printf("%d: %s\n", i, n)
+	}
+}
+
+// 计算数组的平均值
+func calculateAverage() {
+	numbers := [3]float64{71.8, 56.2, 89.5}
+	var sum float64
+	for _, number := range numbers {
+		sum += number
+	}
+	sampleCount := float64(len(numbers))
+	fmt.Printf("Average: %.2f\n", sum/sampleCount)
+
+}
+
 func main() {
-	verifyGolangPackageName()
+	//verifyGolangPackageName()
+	iterateArray()
+	calculateAverage()
 }
