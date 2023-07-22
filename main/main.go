c88a95daebd9e3ade4adee07f3ec1444cfaa7f61
likl <likl@thunisoft.com>
2023-07-22 21:17:25 +0800

    :hammer: 验证GoLang包命名规范方法单独提取一个函数

--- main/main.go
+++ main/main.go
@@ -8,9 +8,13 @@ import (
 	"strconv"
 )
 
-func main() {
+func verifyGolangPackageName() {
 	// 验证包名是否可以带中横线和下划线
 	fmt.Println("Current Year is : " + strconv.Itoa(timeutils.GetYear()))
 	fmt.Println("NickName is : " + name_utils.GetNickName())
 	fmt.Println("Age is : " + strconv.Itoa(age_utils.GetAge()))
+}
+
+func main() {
+	verifyGolangPackageName()
 }
