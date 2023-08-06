574419ec24e4eb96056e78e4df9f22261537a0b5
EndlessOverlook <endlessoverlook@sina.com>
2023-08-06 23:06:15 +0800

    :hammer: 增加TCP/UDP/HTTP服务端和客户端代码

--- main/main.go
+++ main/main.go
@@ -3,13 +3,11 @@ package main
 import (
 	"fmt"
 	"golang-utils/age-utils"
-	"golang-utils/channelutils"
 	"golang-utils/gadget"
 	"golang-utils/name_utils"
 	"golang-utils/timeutils"
 	"math"
 	"strconv"
-	"time"
 )
 
 // 验证包名是否可以带中横线和下划线
@@ -125,20 +123,22 @@ func main() {
 	// }
 
 	// channelutils.TestChannel2()
-	// 两个通道
-	output1 := make(chan string)
-	output2 := make(chan string)
-	// 跑两个子协程，写数据
-	go channelutils.TestSelect1(output1)
-	go channelutils.TestSelect2(output2)
-	// 使用select监听
-	select {
-	case s1 := <-output1:
-		fmt.Println("s1=", s1)
-	case s2 := <-output2:
-		fmt.Println("s2=", s2)
-	}
-	time.Sleep(time.Second * 6)
+
+	// // 两个通道
+	// output1 := make(chan string)
+	// output2 := make(chan string)
+	// // 跑两个子协程，写数据
+	// go channelutils.TestSelect1(output1)
+	// go channelutils.TestSelect2(output2)
+	// // 使用select监听
+	// select {
+	// case s1 := <-output1:
+	// 	fmt.Println("s1=", s1)
+	// case s2 := <-output2:
+	// 	fmt.Println("s2=", s2)
+	// }
+	// time.Sleep(time.Second * 6)
+
 }
 
 type Vec2 struct {
