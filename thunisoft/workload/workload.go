8ba43e37b5b528f6b9230a2732ad72c4b9c4349d
EndlessOverlook <1664864496@qq.com>
2023-12-11 11:01:15 +0800

    重构工作量分配计算工具方法

--- thunisoft/workload/workload.go
+++ thunisoft/workload/workload.go
@@ -14,19 +14,19 @@ const (
 
 // CalculateWorkloadDistribution 计算工作量分配
 func CalculateWorkloadDistribution(totalWorkload float64) {
-	bestResult := math.Inf(1)
-	bestP5DeveloperDayCount, bestP4DeveloperDayCount, bestP3DeveloperDayCount, bestP2DeveloperDayCount := 0.0, 0.0, 0.0, 0.0
+	proximalWorkload := math.Inf(1)
+	proximalP5DeveloperDayCount, proximalP4DeveloperDayCount, proximalP3DeveloperDayCount, proximalP2DeveloperDayCount := 0, 0, 0, 0
 
-	for i := 0.0; i <= totalWorkload/p5DeveloperCostPerDay; i++ {
-		for j := 0.0; j <= totalWorkload/p4DeveloperCostPerDay; j++ {
-			for k := 0.0; k <= totalWorkload/p3DeveloperCostPerDay; k++ {
-				for l := 0.0; l <= totalWorkload/p2DeveloperCostPerDay; l++ {
-					tempWorkload := p5DeveloperCostPerDay*i + p4DeveloperCostPerDay*j + p3DeveloperCostPerDay*k + p2DeveloperCostPerDay*l
+	for i := 0; i <= int(totalWorkload/p5DeveloperCostPerDay); i++ {
+		for j := 0; j <= int(totalWorkload/p4DeveloperCostPerDay); j++ {
+			for k := 0; k <= int(totalWorkload/p3DeveloperCostPerDay); k++ {
+				for l := 0; l <= int(totalWorkload/p2DeveloperCostPerDay); l++ {
+					tempWorkload := p5DeveloperCostPerDay*float64(i) + p4DeveloperCostPerDay*float64(j) + p3DeveloperCostPerDay*float64(k) + p2DeveloperCostPerDay*float64(l)
 					if tempWorkload <= totalWorkload {
 						workloadDiff := totalWorkload - tempWorkload
-						if workloadDiff < bestResult {
-							bestResult = workloadDiff
-							bestP5DeveloperDayCount, bestP4DeveloperDayCount, bestP3DeveloperDayCount, bestP2DeveloperDayCount = i, j, k, l
+						if workloadDiff < proximalWorkload {
+							proximalWorkload = workloadDiff
+							proximalP5DeveloperDayCount, proximalP4DeveloperDayCount, proximalP3DeveloperDayCount, proximalP2DeveloperDayCount = i, j, k, l
 						}
 					}
 				}
@@ -34,6 +34,10 @@ func CalculateWorkloadDistribution(totalWorkload float64) {
 		}
 	}
 
-	fmt.Println("=====[工作量分配计算]=====")
-	fmt.Printf("目标工作量值为%.2f, 最接近的结果为%.2f * %.f(P5) + %.2f * %.f(P4) + %.2f * %.f(P3) + %.2f * %.f(P2) = %.2f\n", totalWorkload, p5DeveloperCostPerDay, bestP5DeveloperDayCount, p4DeveloperCostPerDay, bestP4DeveloperDayCount, p3DeveloperCostPerDay, bestP3DeveloperDayCount, p2DeveloperCostPerDay, bestP2DeveloperDayCount, p5DeveloperCostPerDay*bestP5DeveloperDayCount+p4DeveloperCostPerDay*bestP4DeveloperDayCount+p3DeveloperCostPerDay*bestP3DeveloperDayCount+p2DeveloperCostPerDay*bestP2DeveloperDayCount)
+	bestWorkload := p5DeveloperCostPerDay*float64(proximalP5DeveloperDayCount) + p4DeveloperCostPerDay*float64(proximalP4DeveloperDayCount) + p3DeveloperCostPerDay*float64(proximalP3DeveloperDayCount) + p2DeveloperCostPerDay*float64(proximalP2DeveloperDayCount)
+
+	fmt.Println("=====[工作量分配计算-开始]=====")
+	fmt.Printf("目标工作量值为[%.2f]\n", totalWorkload)
+	fmt.Printf("最接近的结果为[%.2f] = %.2f * %d(P5) + %.2f * %d(P4) + %.2f * %d(P3) + %.2f * %d(P2)\n", bestWorkload, p5DeveloperCostPerDay, proximalP5DeveloperDayCount, p4DeveloperCostPerDay, proximalP4DeveloperDayCount, p3DeveloperCostPerDay, proximalP3DeveloperDayCount, p2DeveloperCostPerDay, proximalP2DeveloperDayCount)
+	fmt.Println("=====[工作量分配计算-结束]=====")
 }
